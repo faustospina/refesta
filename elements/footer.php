abc76feab0909cce11be0736ce120f73e20eb533
Fausto <fast_004@hotmail.com>
2017-04-10 13:52:19 +0000

    primer commit de ensayo

---
+++ elements/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Particles.js</title>
+  
+  
+  
+      <link rel="stylesheet" href="/css/particles.css">
+
+  
+</head>
+
+<body>
+  <div id="particles-js"> <h1>Refesta</h1>
+</div>
+  
+    <script src="/js/particles.js"></script>
+
+</body>
+</html>
+
+
+
+ 
---
+++ view/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hola Mundo!</title>
+    </head>
+    <body>
+       <div id="playlist" style="width: 33.33%; height: 250px;float:left; text-align:center">
+           <h1>playlist</h1>
+       </div>
+       <div id="pantalla" style="width: 33.33%; height: 250px; float:left; text-align:center">
+           <h1>espacio para el frame del video</h1>
+       </div>
+       <div id="reproduccion" style="width: 33.33%; height: 250px;float:left; text-align:center">
+           <h1>Lista que se va reproduciendo</h1>
+       </div>
+       
+    </body>
+   
+    <footer>
+      
+    </footer>
+</html>
