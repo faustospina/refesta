ecfaf2b8a5197cc8503918c332ed3d46ffe074c8
Fausto <fast_004@hotmail.com>
2017-04-10 16:19:33 +0000

    front end 1.0.0

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+	<title>Refesta</title>
+</head>
+<body>
+
+		<div class="container">
+		<div id="stats_div" style="width: 50%; height: 250px;float:left;text-align:center;text-size:80px;color:red">
+			<h1>BUSCAR CANCION</h1>
+		<ul class="list-group">
+ 		 <li class="list-group-item">First item</li>
+ 		 <li class="list-group-item">Second item</li>
+		 <li class="list-group-item">Third item</li>
+		</ul>
+		</div>
+		<div id="lineChart_div" style="width: 50%; height: 250px;float:left;text-align:center;;color:red">
+			<h1>VIDEO DE REPRODUCCION</h1>
+			
+			<br></br>
+			
+			<iframe width="300" height="300" src="https://www.youtube.com/embed/7QU1nvuxaMA" frameborder="0" allowfullscreen></iframe>
+	
+			<h1>LISTA DE REPRODUCCION</h1>
+		 <ul class="list-group">
+ 		 <li class="list-group-item">First item</li>
+ 		 <li class="list-group-item">Second item</li>
+		 <li class="list-group-item">Third item</li>
+		</ul>
+	
+		</div>
+	
+	
+		</div>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+		<br></br>
+	
+
+</body>
+<footer>
+	<?php include 'inc/footer.php'; ?>
+
+</footer>
+</html>
